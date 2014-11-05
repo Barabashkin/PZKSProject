f3b32d3987297dd8e2de68ed32acd5a761dff667
barabashka <ericbarashkin@gmail.com>
2014-11-05 04:15:04 +0200

    not very pretty, quick solutions

--- Web-UI/js/script.js
+++ Web-UI/js/script.js
@@ -23,16 +23,23 @@ $(document).ready(function () {
                 crossDomain: true,
                 complete: function (data) {
                     if (data.responseJSON.code == "0") {
-                        $("#expression").css("border-color","white");
-                        $("#variables").css("border-color","white");
+                        $("#expression").css("border-color", "white");
+                        $("#variables").css("border-color", "white");
                         $("#title").html("Result:");
+                        $("#result").html("");
                         $("#result").append("<p>" + data.responseJSON.result + "</p>");
+                        $("#title1").html("Tree:");
+                        $("#result1").html("");
+                        $("#result1").append("<p>" + data.responseJSON.tree + "</p>");
+
                     }
                     if (data.responseJSON.code == "1") {
                         $("#title").html("");
                         $("#result").html("");
+                        $("#title1").html("");
+                        $("#result1").html("");
                         $("#expression").css("border-color", "red");
-                        $("#variables").css("border-color","white");
+                        $("#variables").css("border-color", "white");
                         swal({
                             title: "Expression error!",
                             text: data.responseJSON.exception,
@@ -44,6 +51,8 @@ $(document).ready(function () {
                     if (data.responseJSON.code == "2") {
                         $("#title").html("");
                         $("#result").html("");
+                        $("#title1").html("");
+                        $("#result1").html("");
                         $("#expression").css("border-color", "white");
                         $("#variables").css("border-color", "red")
                         swal({
@@ -55,20 +64,24 @@ $(document).ready(function () {
                         });
                     }
                     if (data.responseJSON.code == "3") {
-                        $("#expression").css("border-color","white");
-                        $("#variables").css("border-color","white");
+                        $("#expression").css("border-color", "white");
+                        $("#variables").css("border-color", "white");
                         $("#title").html("Exception:");
+                        $("#title1").html("");
+                        $("#result1").html("");
                         var exceptions = data.responseJSON.exception.split(";");
                         exceptions.forEach(function (exception) {
                             $("#result").append("<p>" + exception + "</p>");
                         });
                     }
                     if (data.responseJSON.code == "4") {
-                        $("#expression").css("border-color","white");
-                        $("#variables").css("border-color","white");
+                        $("#expression").css("border-color", "white");
+                        $("#variables").css("border-color", "white");
                         $("#title").html("Exception:");
                         $("#result").html("");
                         $("#result").append("<p>" + data.responseJSON.exception + "</p>");
+                        $("#title1").html("");
+                        $("#result1").html("");
                     }
                 }
             });
